86c29980a0bd61eaaf534781a13692cb38619a81
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2022-03-05 18:23:58 +0100

    Test displayMessage.js

--- js/components/common/displayMessage.js
+++ js/components/common/displayMessage.js
@@ -1,5 +1,4 @@
 export default function displayMessage(messageType, message, targetElement) {
-    let message = "Could not get information";
     const element = document.querySelector(targetElement);
 
     element.innerHTML = `<div class="message ${messageType}">${message}</div>`;
