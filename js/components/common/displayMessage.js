9cb3b61907281d5d1163a43bc5a342985c6996e5
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2022-03-05 18:28:17 +0100

    Test displayMessage.js

--- js/components/common/displayMessage.js
+++ js/components/common/displayMessage.js
@@ -1,6 +1,5 @@
 export default function displayMessage(messageType, message, targetElement) {
     const element = document.querySelector(targetElement);
 
-    // element.innerHTML = `<div class="message ${messageType}">${message}</div>`;
-    // ^ Commented out for now to not get displayed before API is deployed.
+    element.innerHTML = `<div class="message ${messageType}">${message}</div>`;
 }
