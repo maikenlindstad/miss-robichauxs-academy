26e30153c71231f6a963dd1a97d1e9e43b3df3c1
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2023-01-20 21:57:57 +0100

    Update info

--- js/components/common/createFooter.js
+++ js/components/common/createFooter.js
@@ -2,5 +2,5 @@ const footer = document.querySelector("footer");
 
 footer.innerHTML = `
   <a href="https://americanhorrorstory.fandom.com/wiki/Miss_Robichaux%27s_Academy" target="_blank"><p>Miss Robichaux's Academy</p></a> 
-  <p>Copyright 2022</p>
+  <p>Developed by Maiken Lindstad</p>
   <a href="https://americanhorrorstory.fandom.com/wiki/Camp_Redwood" target="_blank"><p>CAMP REDWOOD</p></a>`;
